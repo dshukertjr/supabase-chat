397ff772dc6297c4583e77fd5e447b2543d51759
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-23 19:48:25 +0900

    added dependencies

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> redirect() async {
+    /// Check Auth State
+  }
+}
