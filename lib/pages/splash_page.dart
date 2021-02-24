1159c0bb226ce9e5f136bcfbc355cb1708cd2dc3
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-24 20:45:12 +0900

    checking auth state

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:supabase/supabase.dart';
+import 'package:supabasechat/pages/chat_page.dart';
+import 'package:supabasechat/supabase_provider.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -22,5 +25,14 @@ class _SplashPageState extends State<SplashPage> {
 
   Future<void> redirect() async {
     /// Check Auth State
+    final supabase = SupabaseProvider.instance;
+    final authUser = supabase.auth.currentUser;
+    if (authUser == null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => ChatPage(),
+        ),
+      );
+    }
   }
 }
