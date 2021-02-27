5464dc447e40f64f4f46d8c4b69168b5485a765b
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-27 15:56:32 +0900

    created the tables

---
+++ lib/models/avatar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class Avatar {
+  final String id;
+  final String url;
+
+  Avatar({
+    @required this.id,
+    @required this.url,
+  });
+}
--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:supabasechat/constants.dart';
 
-class EditProfilePage extends StatelessWidget {
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  TextEditingController _nameController;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: 20,
+        ),
+        children: [
+          TextFormField(
+            controller: _nameController,
+            decoration: const InputDecoration(
+              labelText: 'User Name',
+            ),
+            validator: (val) {
+              if (val.isEmpty) {
+                return 'Required';
+              }
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _nameController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _getAllAvatars() async {
+    final res = await supabase.from('avatars').select().execute();
   }
 }
